9b8bfe992b68539f446be9e5a9dd907ef780f860
yordanos legesse <yordanoslegesse15@gmail.com>
2024-08-03 15:35:57 +0300

    time added in the modal

--- task_manager_with_mongo/models/tasks_models.go
+++ task_manager_with_mongo/models/tasks_models.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"time"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -8,6 +10,6 @@ type Task struct {
 	ID          primitive.ObjectID `bson:"_id,omitempity" json:"id" `
 	Title       string             `json:"title" `
 	Description string             `json:"description" `
-	// DueDate     time.Time          `json:"due_date" `
+	DueDate     time.Time          `json:"due_date" `
 	Status      string             `json:"status"`
 }
